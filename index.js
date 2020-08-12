5c80c618d2a1d9a12ddad0cca0112c3937a38cc8
Roman Demikhov <demikhovroman@gmail.com>
2020-08-12 09:27:26 +0300

    done

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
+const refs = {
+  secsRef: document.querySelector('[data-value="secs"]'),
+  minsRef: document.querySelector('[data-value="mins"]'),
+  hoursRef: document.querySelector('[data-value="hours"]'),
+  daysRef: document.querySelector('[data-value="days"]'),
+};
+const timer = {
+  start() {
+    const startTime = new Date("Oct 31, 2020");
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
+      // console.log("startTime", startTime);
+      // console.log("currentTime", currentTime);
+      //   console.log(deltaTime);
+      timerGeneral(deltaTime);
+    }, 1000);
+  },
+};
+timer.start();
 
+function timerGeneral(time) {
+  /*
+   * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+   * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+   */
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
 
+  /*
+   * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+   * остатка % и делим его на количество миллисекунд в одном часе
+   * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+   */
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
 
+  /*
+   * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+   * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+   */
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
 
+  /*
+   * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+   * миллисекунд в одной секунде (1000)
+   */
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  // console.log(`${days}:${hours}:${mins}:${secs}`);
 
+  refs.secsRef.textContent = secs;
+  refs.minsRef.textContent = mins;
+  refs.hoursRef.textContent = hours;
+  refs.daysRef.textContent = days;
+}
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
