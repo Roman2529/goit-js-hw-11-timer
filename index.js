611aa4044863149c4352c3809773cd5e5e402cb6
Roman Demikhov <demikhovroman@gmail.com>
2020-08-17 20:17:03 +0300

    fix

--- index.js
+++ index.js
@@ -16,40 +16,26 @@ const timer = {
   },
 };
 timer.start();
-
+let days;
+let hours;
+let mins;
+let secs
 function timerGeneral(time) {
-  /*
-   * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-   * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-   */
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-
-  /*
-   * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-   * остатка % и делим его на количество миллисекунд в одном часе
-   * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-   */
-  const hours = pad(
+  days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  hours = pad(
     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
   );
-
-  /*
-   * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-   * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-   */
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-
-  /*
-   * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-   * миллисекунд в одной секунде (1000)
-   */
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
+  mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  return timerValue();
+};
+function timerValue() {
   refs.secsRef.textContent = secs;
   refs.minsRef.textContent = mins;
   refs.hoursRef.textContent = hours;
   refs.daysRef.textContent = days;
-}
+};
+
 
 function pad(value) {
   return String(value).padStart(2, "0");
